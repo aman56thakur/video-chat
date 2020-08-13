76f1c649bad80ac4ac59466984c0544187ac3819
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-14 01:27:10 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
-  host: "/",
-  port: "9000",
+  host: '/',
+  port: 443,
 })
 
 const myVideo = document.createElement('video')
